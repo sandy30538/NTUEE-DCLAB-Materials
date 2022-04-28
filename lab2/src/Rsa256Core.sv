fba42da79cc38568acb6958c2a0206d547935b79
sandy30538 <b03901160@ntu.edu.tw>
2022-04-28 12:14:30 +0800

    Add files via upload in 2022.04.28

---
+++ lab2/src/Rsa256Core.sv
@@ -0,0 +1,15 @@
+module Rsa256Core (
+	input          i_clk,
+	input          i_rst,
+	input          i_start,
+	input  [255:0] i_a, // cipher text y
+	input  [255:0] i_d, // private key
+	input  [255:0] i_n,
+	output [255:0] o_a_pow_d, // plain text x
+	output         o_finished
+);
+
+// operations for RSA256 decryption
+// namely, the Montgomery algorithm
+
+endmodule
